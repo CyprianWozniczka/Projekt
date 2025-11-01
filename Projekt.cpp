462aa0502903ee2ef9267acacec9ab8538ddd161
CyprianWozniczka <cyprian.wa@gmail.com>
2025-11-01 18:47:31 +0100

    finished

--- Projekt.cpp
+++ Projekt.cpp
@@ -12,49 +12,6 @@ float CtoK(temp);
 float KtoF(temp);
 float KtoC(temp);
 
-float FtoC(temp){
-    float tempFC=(temp-32.0)*5.0/9.0
-    if (tempFC<-273.15)
-        cout << "-9999999" << endl;
-    else
-        cout << tempFC << "C" << endl;
-}
-float FtoK(temp){
-    float tempFK=(temp+459.67)*5.0/9.0
-    if (tempFK<0)
-        cout << "-9999999" << endl;
-    else
-        cout << tempFK << "K" << endl;
-}
-float CtoF(temp){
-    float tempCF=temp*5.0/9.0+32.0
-    if (tempCF<-459.67)
-        cout << "-9999999" << endl;
-    else
-        cout << tempCF << "F" << endl;
-}
-float CtoK(temp){
-    float tempCK=temp+273.15
-    if (tempCK<0)
-        cout << "-9999999" << endl;
-    else
-        cout << tempCK << "K" << endl;
-}
-float KtoF(temp){
-    float tempKF=temp*5.0/9.0-459.67
-    if (tempKF<-459.67)
-        cout << "-9999999" << endl;
-    else
-        cout << tempKF << "F" << endl;
-}
-float KtoC(temp){
-    float tempKC=temp-273.15
-    if (tempKC<-273.15)
-        cout << "-9999999" << endl;
-    else
-    cout << tempKC << "C" << endl;
-}
-
 int main(){
     cout << "1 - przelicz Fahr -> Celsius" << endl;
     cout << "2 - przelicz Fahr -> Kelwin" << endl;
@@ -63,36 +20,79 @@ int main(){
     cout << "5 - przelicz  Kelwin -> Celsius" << endl;
     cout << "6 - przelicz Kelwin -> Fahr" << endl;
     cout << "7 - zakończ działanie programu" << endl;
-    cin >> choice >> endl;
+    cin >> choice;
 
     switch (choice){
         case 1:
-        float FtoC(temp);
+            FtoC(temp);
         break;
 
         case 2:
-        float FtoK(temp);
+            FtoK(temp);
         break;
 
         case 3:
-        float CtoF(temp);
+            CtoF(temp);
         break;
 
         case 4:
-        float CtoK(temp);
+            CtoK(temp);
         break;
 
         case 5:
-        float KtoC(temp);
+            KtoC(temp);
         break;
 
         case 6:
-        float KtoF(temp);
+            KtoF(temp);
         break;
 
         default:
-        return 0;
+            return 0;
     }
     
     system("cls");
+}
+
+FtoC(temp){
+    float tempFC=(temp-32.0)*5.0/9.0
+    if (tempFC<-273.15)
+        cout << "-9999999" << endl;
+    else
+        cout << tempFC << "C" << endl;
+}
+FtoK(temp){
+    float tempFK=(temp+459.67)*5.0/9.0
+    if (tempFK<0)
+        cout << "-9999999" << endl;
+    else
+        cout << tempFK << "K" << endl;
+}
+CtoF(temp){
+    float tempCF=temp*5.0/9.0+32.0
+    if (tempCF<-459.67)
+        cout << "-9999999" << endl;
+    else
+        cout << tempCF << "F" << endl;
+}
+CtoK(temp){
+    float tempCK=temp+273.15
+    if (tempCK<0)
+        cout << "-9999999" << endl;
+    else
+        cout << tempCK << "K" << endl;
+}
+KtoF(temp){
+    float tempKF=temp*5.0/9.0-459.67
+    if (tempKF<-459.67)
+        cout << "-9999999" << endl;
+    else
+        cout << tempKF << "F" << endl;
+}
+KtoC(temp){
+    float tempKC=temp-273.15
+    if (tempKC<-273.15)
+        cout << "-9999999" << endl;
+    else
+    cout << tempKC << "C" << endl;
 }

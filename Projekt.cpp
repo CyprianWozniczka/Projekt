d18431a6ed215cc35227970e99e490f30a7356e6
CyprianWozniczka <cyprian.wa@gmail.com>
2025-11-01 17:18:53 +0100

    defined functions

--- Projekt.cpp
+++ Projekt.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <h.studio>
-using namespace std
+using namespace std;
+
+int temp, unit;
+int KtoF(temp,unit);
+int KtoC(temp,unit);
+int CtoF(temp,unit);
+int CtoK(temp,unit);
+int FtoC(temp,unit);
+int FtoK(temp,unit);
+
